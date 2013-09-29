3c8319aa0205dda30ab92d546683674dd4f6fe10
Sherlock <sherlock.z0917@gmail.com>
2013-09-29 20:25:58 +0800

    almost finish

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,24 +3,35 @@ var GetNewsMain = {
 		GetNewsMain.bindEvent();
 	},
 	bindEvent : function(){
-		$('li').bind('tap',GetNewsMain.getList());
-		$('.back-btn').bind('tap',function(){ history.go(-1); });
-	}
+		$('.nav-btn').bind('tap',GetNewsMain.getList());
+		$('#news-list-more').bind('tap',GetNewsMain.getListMore());
+	},
 	getList : function(){
 		return function (e){
-			$.getJSON('/list/' + $(e.target).attr('urlstr') + '/' + $(e.target).attr('nextpage'),
-			function(result){
-				$.each(result, function(i, item){
-					var link = $(Template.news_link);
-					link.find('.news-link').attr('urlstr',item.link);
-					link.find('.news-title').text(item.title);
-					link.find('.news-from').text(item.from);
-					link.find('.news-date').text(item.date);
-					link.bind('tap',GetNewsMain.getArticle());
-					$('#news-list').append(link);
-				});
-			});
+			$('#news-list-title').text($(e.target).text());
+			$('#news-list-more').attr('urlstr', $(e.target).attr('urlstr')).attr('nextpage', parseInt($(e.target).attr('nextpage')) + 1);
+			GetNewsMain.getListAjax(e);
 		}
+	},
+	getListMore : function(){
+		return function (e){
+			GetNewsMain.getListAjax(e);
+			$('#news-list-more').attr('nextpage', parseInt($('#news-list-more').attr('nextpage')) + 1);
+		}
+	},
+	getListAjax : function(e){
+		$.getJSON('/list/' + $(e.target).attr('urlstr') + '/' + $(e.target).attr('nextpage'),
+		function(result){
+			$.each(result, function(i, item){
+				var link = $(Template.news_link);
+				link.find('.news-link').attr('urlstr',item.link);
+				link.find('.news-title').text(item.title);
+				link.find('.news-from').text(item.from + ':');
+				link.find('.news-date').text(item.date);
+				link.bind('tap',GetNewsMain.getArticle());
+				$('#news-list').append(link);
+			});
+		});
 	},
 	getArticle : function(){
 		return function (e){
@@ -28,7 +39,12 @@ var GetNewsMain = {
 			function(result){
 				$('#news-article-title').text(result.title);
 				$('#news-article-content').html(result.content);
-				$('#news-article-content').find('img').remove();
+				var img = $('#news-article-content').find('img');
+				for(var i = 0; i < img.length; i++){
+					var srcStr = 'http://info.scau.edu.cn/' + $(img[i]).attr('src');
+					$(img[i]).attr('src', srcStr);
+					console.log($(img[i]).css('margin-left'));
+				}
 			});
 		}
 	}
--- public/javascripts/template.js
+++ public/javascripts/template.js
@@ -1,11 +1,12 @@
 var Template = {
 	news_link : [
-	'<li>',
+	'<li class="news-link-btn">',
 		'<a href="#news-article-page" class="news-link">',
-			'<h2 class="news-title"></h2>',
-			'<h3 class="news-from"></h3>',
-			'<h3 class="news-date"></h3>',
+			'<h4 class="news-from"></h4>',
+			'<h3 class="news-title"></h3>',
+			'<h5 class="news-date"></h5>',
 		'</a>',
+		'<div class="tail-border"><div class="tail-inner"></div></div>',
 	'</li>'
 	].join('')
 }
