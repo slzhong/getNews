1f0cd07a5298bc80353b83f6fc975adfcbd8b477
Sherlock <sherlock.z0917@gmail.com>
2013-09-30 21:49:32 +0800

    added a local version

---
+++ local/javascripts/main.js
@@ -0,0 +1,55 @@
+var GetNewsMain = {
+	init : function(){
+		GetNewsMain.bindEvent();
+	},
+	bindEvent : function(){
+		$('.nav-btn').bind('tap',GetNewsMain.getList());
+		$('#news-list-more').bind('tap',GetNewsMain.getListMore());
+	},
+	getList : function(){
+		return function (e){
+			$('#news-list-title').text($(e.target).text());
+			$('#news-list-more').attr('urlstr', $(e.target).attr('urlstr')).attr('nextpage', parseInt($(e.target).attr('nextpage')) + 1);
+			GetNewsMain.getListAjax(e);
+		}
+	},
+	getListMore : function(){
+		return function (e){
+			GetNewsMain.getListAjax(e);
+			$('#news-list-more').attr('nextpage', parseInt($('#news-list-more').attr('nextpage')) + 1);
+		}
+	},
+	getListAjax : function(e){
+		$.getJSON('http://localhost:3000/list/' + $(e.target).attr('urlstr') + '/' + $(e.target).attr('nextpage'),
+		function(result){
+			$.each(result, function(i, item){
+				var link = $(Template.news_link);
+				link.find('.news-link').attr('urlstr',item.link);
+				link.find('.news-title').text(item.title);
+				link.find('.news-from').text(item.from + ':');
+				link.find('.news-date').text(item.date);
+				link.bind('tap',GetNewsMain.getArticle());
+				$('#news-list').append(link);
+			});
+		});
+	},
+	getArticle : function(){
+		return function (e){
+			$.getJSON('http://localhost:3000/article' + $(e.target).parent('.news-link').attr('urlstr'),
+			function(result){
+				$('#news-article-title').text(result.title);
+				$('#news-article-content').html(result.content);
+				var img = $('#news-article-content').find('img');
+				for(var i = 0; i < img.length; i++){
+					var srcStr = 'http://info.scau.edu.cn/' + $(img[i]).attr('src');
+					$(img[i]).attr('src', srcStr);
+					console.log($(img[i]).css('margin-left'));
+				}
+			});
+		}
+	}
+}
+
+window.onload = function(){
+	GetNewsMain.init();
+}
---
+++ local/javascripts/template.js
@@ -0,0 +1,12 @@
+var Template = {
+	news_link : [
+	'<li class="news-link-btn">',
+		'<a href="#news-article-page" class="news-link">',
+			'<h4 class="news-from"></h4>',
+			'<h3 class="news-title"></h3>',
+			'<h5 class="news-date"></h5>',
+		'</a>',
+		'<div class="tail-border"><div class="tail-inner"></div></div>',
+	'</li>'
+	].join('')
+}
